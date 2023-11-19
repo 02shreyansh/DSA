41feaae87289a003487437b4f2549c81969055ce
02shreyansh <02.shreyansh.10@gmail.com>
2023-11-19 14:01:05 +0530

    Day 1 of DSA

--- day1.cpp
+++ day1.cpp
@@ -46,3 +46,53 @@ int main(){
     cout<<"decimal to binary is: "<<ans;
     
 }
+
+
+// binary to decimal
+#include <iostream>
+using namespace std;
+int power(int a,int b){
+    int ans=1;
+    for(int i=1;i<=b;i++){
+        ans=ans*a;
+
+    }
+    return ans;
+}
+int main(){
+    int n;
+    cout<<"enter number: "<<endl;
+    cin>>n;
+    int i=0;
+   
+    int ans=0;
+    while(n!=0){
+        int digit=n%10;
+        if(digit==1){
+            ans+=power(2,i);
+        }
+        i++;
+        n=n/10;
+
+    }
+    cout<<"binary to decimal is: "<<ans;
+}
+
+// compliment of base 10
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number: "<<endl;
+    cin>>n;
+    int mask=1;
+    while(mask<n){
+        mask=(mask<<1)|1;//left shift and bitwise or
+
+    }
+    cout<<"Answer is: "<<(n^mask);
+}
+
+
+
+
